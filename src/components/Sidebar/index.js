955b2092d11fbbb2f69a6350b95f9e10f8c136cd
Walter Franz <walter.franz@gmail.com>
2023-02-13 14:02:22 -0500

    sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,15 +1,22 @@
-import {Link} from 'react-router-dom'
+import {Link, NavLink} from 'react-router-dom'
 import './index.scss'
-import LogoS from '../../assets/images/Logo-s-png'
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome } from '@fortawesome/free-solid-svg-icons'
 
 
 
 const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='Logo' to ='/'>
-            <img srr={LogoS} alt="Logo" />
-            <img srr={LogoS} alt="Logo" />
+    <div className="nav-bar'">
+        <Link className="logo" to ="/">
+            <img src={LogoS} alt="logo" />
+            <img className="sub-logo"src={LogoSubtitle} alt="Slobodan" />
         </Link>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+            </NavLink>
+
     </div>
 )
 
